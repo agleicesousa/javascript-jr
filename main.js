d37c339e77e9addc31b6371cb7139743093ee658
Agleice Sousa <agleicesousa98@gmail.com>
2026-07-29 18:22:20 -0300

    add: usando a variável 'const"

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 console.log('Hello, World!');
 
-//* let, const ou var
+//* LET, CONST e VAR
 
-//  let - É possivel alterar no futuro
+//  let - É possível alterar no futuro
 
 //  Ex¹
 let numero = 8;
 console.log(numero);
-
 numero = 25;    //  Alteração do valor da variável
 console.log(numero);
 
 //  Ex²
-let nome = Gleice;
+let nome = "Gleice";
 console.log(nome)
-
-nome = Fátima;
+nome = "Fátima";
 console.log(nome)   //  Outra alteração de valor
+
+
+// const - Valor não mutável, a não ser que mude diretamente na variável correspondente
+//! OBS: Para testar ambos os resultados descomente o exemplo que gostaria de usar removendo os '/*  */' correspondentes
+
+/*
+//  Ex¹
+const lugar = "Brasil";
+console.log(lugar)
+lugar = "Portugal";     // Vai dar erro no console.
+console.log(lugar)
+*/
+
+/*
+//  Ex²
+const saldo = 3.14;
+console.log(saldo)
+saldo = 9.34            // Outro erro no console
+console.log(saldo)
+*/
