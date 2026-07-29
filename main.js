191f8a2ff081d375b0102c0f3854f3f181cdc329
Agleice Sousa <agleicesousa98@gmail.com>
2026-07-29 18:07:22 -0300

    add: usos da variável 'let'

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log('Hello, World!');
+
+//* let, const ou var
+
+//  let - É possivel alterar no futuro
+
+//  Ex¹
+let numero = 8;
+console.log(numero);
+
+numero = 25;    //  Alteração do valor da variável
+console.log(numero);
+
+//  Ex²
+let nome = Gleice;
+console.log(nome)
+
+nome = Fátima;
+console.log(nome)   //  Outra alteração de valor
