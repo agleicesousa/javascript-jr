7f84b525197c9098e83b041e3bb6839c7f889e13
Agleice Sousa <agleicesousa98@gmail.com>
2026-07-30 12:01:08 -0300

    add: exemplos de uso do método 'concat()'

--- variaveis.js
+++ variaveis.js
@@ -100,3 +100,16 @@ console.log(primeiroNome + " " + segundoNome);
 let numeroUm = 33;
 let numeroDois = 34;
 console.log(numeroUm + numeroDois);
+
+
+//* OPERADOR +=
+let texto = "Olá";
+texto += ", Usuário!";
+console.log(texto);
+
+
+//* MÉTODO concat()
+let palavraUm = "Deu";
+let palavraDois = "certo!";
+let fraseCompleta = palavraUm.concat(' ', palavraDois);
+console.log(fraseCompleta);
