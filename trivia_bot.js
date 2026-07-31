36dd918344700727f118bc0c647695e853511464
Agleice Sousa <agleicesousa98@gmail.com>
2026-07-31 14:51:21 -0300

    refator: substituir concatenação de strings por concat()

--- trivia_bot.js
+++ trivia_bot.js
@@ -4,16 +4,26 @@ const botName = "Jarvis";
 const botLocation = "Brazil";
 const favoriteLanguage = "JavaScript";
 
-console.log("My name is " + botName + " and I live on " + botLocation + ".");
-console.log("My favorite programming language is " + favoriteLanguage + ".");
+console.log("My name is ".concat(botName, " and I live on ", botLocation, "."));
+console.log("My favorite programming language is ".concat(favoriteLanguage, "."));
 
-let codingFact = "I can use " + favoriteLanguage + " for both front-end and back-end development.";
+let codingFact = "I can use ".concat(
+  favoriteLanguage,
+  " for both front-end and back-end development."
+);
 console.log(codingFact);
 
-codingFact = favoriteLanguage + " was created in just 10 days.";
+codingFact = "".concat(favoriteLanguage, " was created in just 10 days.");
 console.log(codingFact);
 
-codingFact = "In " + favoriteLanguage + ", functions are also objects.";
+codingFact = "In ".concat(favoriteLanguage, ", functions are also objects.");
 console.log(codingFact);
 
-console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - ".concat(
+    botName,
+    " from ",
+    botLocation,
+    "."
+  )
+);
