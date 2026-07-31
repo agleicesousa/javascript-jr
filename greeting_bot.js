bcc4a950eab2a49f1271514517d85ac41e53ef8f
Agleice Sousa <agleicesousa98@gmail.com>
2026-07-31 14:43:05 -0300

    FEAT: O primeiro exercicio de variáveis

---
+++ greeting_bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Jarvis";
+const botLocation = "Brazil";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "I can use " + favoriteLanguage + " for both front-end and back-end development.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was created in just 10 days.";
+console.log(codingFact);
+
+codingFact = "In " + favoriteLanguage + ", functions are also objects.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
