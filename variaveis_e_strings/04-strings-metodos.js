b50437c439832cf067773ddcfd4b239dddb7eb65
Agleice Sousa <agleicesousa98@gmail.com>
2026-08-08 12:29:26 -0300

    feat: adiciona exemplos de uso dos métodos charCodeAt() e String.fromCharCode() em strings

--- variaveis_e_strings/04-strings-metodos.js
+++ variaveis_e_strings/04-strings-metodos.js
@@ -9,3 +9,23 @@ console.log(posicao); // Retorna 15
 let fraseLonga = "JavaScript é incrível, e JavaScript é poderoso!";
 let proximaPosicao = fraseLonga.indexOf("JavaScript", 10);
 console.log(proximaPosicao); // Retorna 26
+
+
+//? ----------------------------------------------------------  CÓDIGOS ASCII E CARACTERES  ------------------------------------------------------------
+
+//* charCodeAt()
+// Descobre o número (código ASCII/Unicode) por trás de um caractere. O (0) é a posição na string.
+
+// Ex¹ - Letra "A" (maiúscula)
+let letra = "A";
+console.log(letra.charCodeAt(0)); // Retorna 65
+
+// Ex² - Símbolo "!"
+let simbolo = "!";
+console.log(simbolo.charCodeAt(0)); // Retorna 33
+
+//* String.fromCharCode()
+// Faz o caminho inverso: transforma um número no seu caractere correspondente.
+// Ex³ - Código 65
+let caractere = String.fromCharCode(65);
+console.log(caractere); // Retorna "A"
